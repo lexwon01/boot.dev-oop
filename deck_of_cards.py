8e6d1040fb5f347072f95604a7c12e1cfc687309
lexwon <lexwon01@gmail.com>
2025-12-03 08:20:09 +0000

    Added DeckOfCards file

---
+++ deck_of_cards.py
@@ -0,0 +1,47 @@
+import random
+
+
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+
+    def __init__(self):
+        self.__cards = []
+        self.create_deck()
+        
+
+    def create_deck(self):
+        for suite in self.SUITS:
+            for rank in self.RANKS:
+                card_tuple = (rank, suite)
+                self.__cards.append(card_tuple)
+                
+
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
+
+    def deal_card(self):
+        if len(self.__cards) > 0:
+            return self.__cards.pop()
+        else:
+            return None
+
+    # don't touch below this line
+
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
+
